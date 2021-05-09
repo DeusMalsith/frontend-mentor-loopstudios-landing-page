0c95532bd174363fbe060f61a9628df5472a4c41
DeusMalsith <justinrashallphotography@gmail.com>
2021-05-09 02:23:06 -0500

    Completed hamburger button menu

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
+const hamButton = document.querySelector(".ham-button");
+const closeButton = document.querySelector(".close-button");
+const showNav = document.querySelector(".nav-list");
 
-hamburger.addEventListener('click', event => {
-  console.log("clicked");
-});
+hamburger.onclick = () => {
+  hamButton.classList.toggle("displayed");
+  closeButton.classList.toggle("displayed");
+  showNav.classList.toggle("show-nav");
+
+  if (document.body.style.overflowY === "hidden") {
+    document.body.style.overflowY = "auto";
+  } else {
+    document.body.style.overflowY = "hidden";
+  }
+};
